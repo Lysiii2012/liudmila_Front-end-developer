b4a625038c47d61aaba30a273dbbd3655b302844
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-02-05 16:40:30 +0200

    gf

--- js/script.js
+++ js/script.js
@@ -13,60 +13,76 @@ var swiper = new Swiper('.blog-slider', {
         clickable: true,
       }
     });
-
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
+      // Обработчик для выделения активной ссылки
+    
+      // Обработчик для меню и его закрытия
+      const drop = document.getElementById("drop");
+      const nav = document.querySelector(".nav-links");
+      const body = document.body;
+    
+      function toggleMenu() {
+        nav.classList.toggle("open");
+        drop.querySelectorAll("span").forEach(i => {
+          i.classList.toggle("open");
+        });
+        body.classList.toggle("hidden");
+      }
+    
+      drop.addEventListener("click", toggleMenu);
+    
+      const navLinksMenu = document.querySelectorAll(".nav-links ~ ul li");
+      navLinksMenu.forEach(function (link) {
+        link.addEventListener("click", function () {
+          toggleMenu();
+          // Добавьте другие действия, если необходимо
+        });
+      });
+    
+      const linkMob = document.querySelectorAll('.nav-links.open a');
+    
+      linkMob.forEach(link => {
+        link.addEventListener("click", function (event) {
+          event.preventDefault();
+          const targetId = link.getAttribute("href").substring(1);
+          const targetElement = document.getElementById(targetId);
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+          }
+             toggleMenu();
+        });
+      });
       let navLinks = document.querySelectorAll(".nav-links a");
     
-      navLinks.forEach(function(link) {
-        link.addEventListener("click", function() {
-          navLinks.forEach(function(link) {
+      navLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+          navLinks.forEach(function (link) {
             link.classList.remove("active");
           });
-    
+          toggleMenu();
           this.classList.add("active");
         });
       });
     });
+    
 
+    // window.addEventListener("scroll", function () {
+    //   var scroll = window.scrollY || document.documentElement.scrollTop;
+    //   var drop = document.getElementById("drop");
+    
+    //   if (scroll > 100) {
+    //     drop.classList.add("active");
+    //   } else if (scroll < 80) {
+    //     drop.classList.remove("active");
+    //   }
+    // });
+    
+      // var drop = document.getElementById("drop");
+      // let nav = document.querySelector(".nav-links");
+      // var body = document.body;
 
-
-
-    window.addEventListener("scroll", function () {
-      var scroll = window.scrollY || document.documentElement.scrollTop;
-      var drop = document.getElementById("drop");
-    
-      if (scroll > 100) {
-        drop.classList.add("active");
-      } else if (scroll < 80) {
-        drop.classList.remove("active");
-      }
-    });
-    
-    function toggleMenu() {
-      var drop = document.getElementById("drop");
-      var nav = document.querySelector(".nav-links");
-      var body = document.body;
-    
-      function handleMenuToggle() {
-        nav.classList.toggle("open");
-        drop.querySelectorAll("span").forEach(i => {
-          i.classList.toggle("open")
-        });
-        body.classList.toggle("hidden");
-        window.scrollTo(0, 0); // cheating
-      }
-    
-      drop.addEventListener("click", handleMenuToggle);
-    
-      var navLinks = document.querySelectorAll(".nav-links ~ ul li");
-      navLinks.forEach(function (link) {
-        link.addEventListener("click", handleMenuToggle);
-      });
-    }
-    
-    // Call the toggleMenu function when the DOM is fully loaded
-    document.addEventListener("DOMContentLoaded", toggleMenu);
-    
+     
+      
 
     // document.addEventListener("scroll", function() {
     //   var scroll = window.scrollY || document.documentElement.scrollTop;
